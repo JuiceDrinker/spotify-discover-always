5c99cf31b8dad25713f20da11394d092448727cf
Adi <subramanianaditya42@gmail.com>
2023-03-28 16:23:06 +0200

    Implement GET /authenticate

---
+++ api/authenticate.ts
@@ -0,0 +1,30 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const scope = "user-read-private user-read-email";
+  const params = {
+    response_type: "code",
+    client_id: process.env.CLIENT_ID || "dummyClientId",
+    scope: scope,
+    redirect_uri: "https://spotify-discover-always.vercel.app/api/hello",
+    state: generateRandomString(16),
+  };
+  console.log(params.client_id);
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      new URLSearchParams(params).toString()
+  );
+};
+
+const generateRandomString = (length: number) => {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    result += chars.charAt(randomIndex);
+  }
+
+  return result;
+};
