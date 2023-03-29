da3e35db2db1a9dddcef724cadd641d7ccff6129
Adi <subramanianaditya42@gmail.com>
2023-03-29 16:26:37 +0200

    Remove log

--- api/authenticate.ts
+++ api/authenticate.ts
@@ -6,6 +6,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   } = req;
 
   if (state !== req.cookies["spotify_auth_state"]) {
+    console.log("here");
     res.status(403).json({ message: "Forbidden" });
   }
 
